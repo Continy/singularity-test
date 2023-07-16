2783b87e8f8370b585228283730901d1827f33d3
Continy <satanama.ring@gmail.com>
2023-07-16 18:07:02 +0800

    bug

--- test.py
+++ test.py
@@ -24,7 +24,6 @@ files.sort()
 for idx, file in enumerate(files):
     #torch加高斯噪声，标准差为0.1，保存
     img = cv2.imread(file)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = torch.from_numpy(img)
     img = img.permute(2, 0, 1)
     img = img.unsqueeze(0)
@@ -33,5 +32,4 @@ for idx, file in enumerate(files):
     noisy_img = noisy_img.squeeze(0)
     noisy_img = noisy_img.permute(1, 2, 0)
     noisy_img = noisy_img.numpy()
-    noisy_img = cv2.cvtColor(noisy_img, cv2.COLOR_RGB2BGR)
     cv2.imwrite('result/noisy_img' + str(idx) + '.png', noisy_img)
